f6c806a57619c38113339e80e1ad3e93d2f09ca9
kim-shun <kimura1417080@gmail.com>
2021-08-15 16:40:11 +0900

    rubocopの実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.last_name}
+    nickname { Faker::Name.last_name }
     last_name { '山田' }
     first_name { '太郎' }
     last_name_kana { 'ヤマダ' }
     first_name_kana { 'タロウ' }
-    birth_date {Faker::Date.birthday}
+    birth_date { Faker::Date.birthday }
     email { Faker::Internet.free_email }
     password = 'a1' + Faker::Internet.password(min_length: 4)
     password { password }
