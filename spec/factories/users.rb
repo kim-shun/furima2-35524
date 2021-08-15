82a89d20c15658402474475bb2a69621ee219282
kim-shun <kimura1417080@gmail.com>
2021-08-15 14:57:18 +0900

    ユーザー情報をFactoryBotで指定

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.last_name}
+    last_name { '山田' }
+    first_name { '太郎' }
+    last_name_kana { 'ヤマダ' }
+    first_name_kana { 'タロウ' }
+    birth_date {Faker::Date.birthday}
+    email { Faker::Internet.free_email }
+    password = 'a1' + Faker::Internet.password(min_length: 4)
+    password { password }
+    password_confirmation { password }
+  end
+end
