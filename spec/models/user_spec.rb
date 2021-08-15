e58e8606f6d159d2ff460c74d0a284d930cba56b
kim-shun <kimura1417080@gmail.com>
2021-08-15 15:09:55 +0900

    ひとまずテストコード記入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる時' do
+      it '全ての項目を入力すれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録できない時' do
+      it 'nicknameが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+    end
+  end
 end
