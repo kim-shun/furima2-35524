00ca2cbf19ebcfeb3a0f50d5f5ec3146de5d0c88
kim-shun <kimura1417080@gmail.com>
2021-08-16 10:32:41 +0900

    ActiveHashを用いたアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :sales_status
+  belongs_to :scheduled_delivery
+  belongs_to :shipping_fee_status
+  
 end
