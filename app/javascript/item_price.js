7ae7ab8e093dde2466f6ffbfff6085fcf8b089a6
kim-shun <kimura1417080@gmail.com>
2021-08-16 11:40:55 +0900

    利益計算結果を表示できるようにする

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    const taxValue = inputValue * 0.1;
+    addTaxDom.innerHTML = Math.floor(taxValue);
+    const profit = document.getElementById("profit");
+    profit.innerHTML = Math.floor(inputValue - taxValue);
+  })
 });
