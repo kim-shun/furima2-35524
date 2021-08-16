f56505e285ff5cf492d370c999e362a73a782e8f
kim-shun <kimura1417080@gmail.com>
2021-08-16 10:44:25 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,19 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee_status
   belongs_to :user
 
+  with_options presence: true do
+    validates :item_name
+    validates :info
+    validates :price
+
+    with_options numericality: { other_than: 1, messag: "can't be blank" } do
+      validates :category_id
+      validates :prefecture_id
+      validates :sales_status_id
+      validates :scheduled_delivery_id
+      validates :shipping_fee_status_id
+    end
+
+  end
+
 end
