a037c559246b8090558fa2ac8240f3611d3eb306
kim-shun <kimura1417080@gmail.com>
2021-08-17 23:21:17 +0900

    データをテーブルに保存する処理を書く

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -16,5 +16,7 @@ class OrderAddress
   validates :phone_number, length: { minimum: 10, maximum: 11}
 
   def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
