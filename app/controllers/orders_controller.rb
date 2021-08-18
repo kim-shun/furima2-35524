885eaeebf3c9fddee43a4fdeb3609bff385b40eb
kim-shun <kimura1417080@gmail.com>
2021-08-18 17:30:58 +0900

    住所を保存できるようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,26 @@
 class OrdersController < ApplicationController
+  before_action :set_order
+
   def index
+    @order_address =OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def set_order
     @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
