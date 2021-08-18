c882b4a5177b432b83bcde93929a31ed97a23a47
kim-shun <kimura1417080@gmail.com>
2021-08-18 23:40:31 +0900

    トークンの生成を確認

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -12,7 +12,14 @@ const pay = () => {
       cvc: formData.get("order_address[cvc]"),
       exp_month: formData.get("order_address[exp_month]"),
       exp_year: `20${formData.get("order_address[exp_year]")}`,
-    }
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token);
+      }
+    });
   })
 };
 
